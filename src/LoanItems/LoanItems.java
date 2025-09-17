f418a9b1290d9c19e89d0e86e50295145eda5255
Namirah0310 <nare0001@stud.ek.dk>
2025-09-17 09:24:21 +0200

    commit01 superclass

--- src/LoanItems/LoanItems.java
+++ src/LoanItems/LoanItems.java
@@ -1,4 +1,35 @@
 package LoanItems;
 
+//superclass
 public class LoanItems {
+    protected String type;
+    protected String title;
+
+    //Constructor
+    public LoanItems(String type, String title) {
+        this.type = type;
+        this.title = title;
+    }
+
+    //Getters
+    public String getType(){
+        return type;
+    }
+    public String getTitle(){
+        return title;
+    }
+
+    //Setters
+    public void setType(String type){
+        this.type = type;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    @Override
+    public String toString(){
+        return " " + type + "\t" + title + "\n";
+    }
+
 }
