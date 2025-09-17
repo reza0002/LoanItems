228297d17a3ddeca67ce717fe1cdf3f78ed218f0
Tamir <tamirhaldlevi@gmail.com>
2025-09-17 09:45:32 +0200

    .

--- src/LoanItems/Library.java
+++ src/LoanItems/Library.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Library {
     public static void main(String[] args) {
+        welcomeMessage();
+        registerItems();
 
     }
     public static void welcomeMessage() {
